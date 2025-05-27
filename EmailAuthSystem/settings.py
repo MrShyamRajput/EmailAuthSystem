a003ef64458194c4468a5c977867a029f5650b7a
MrShyamRajput <144553787+MrShyamRajput@users.noreply.github.com>
2025-05-27 12:23:24 +0530

    whitenose added

--- EmailAuthSystem/settings.py
+++ EmailAuthSystem/settings.py
@@ -40,6 +40,7 @@ EMAIL_HOST_PASSWORD = "hnsx wucy tqwb ppwm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,11 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
